78e54f32dd568923c2b0c46b4ee1aecd70421fc0
NanNo <inhwanpark19991120@gmail.com>
2024-02-19 17:44:59 +0900

    맛보기 Spring

--- todoapp/src/main/java/com/example/todoapp/controller/TodoController.java
+++ todoapp/src/main/java/com/example/todoapp/controller/TodoController.java
@@ -1,12 +1,34 @@
 package com.example.todoapp.controller;
 
+import com.example.todoapp.domain.ToDo;
+import com.example.todoapp.repository.ToDoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor //스프링이 실행하면서 자동으로 데이터를 넣어준다.
 public class TodoController {
+    private final ToDoRepository toDoRepository;
     @GetMapping("/")
-    public String index(){
+    public String index(Model model)
+    {
+        List<ToDo> todos = toDoRepository.findAll();
+        model.addAttribute("todos", todos);
         return "todos";
+
+    }
+
+    @PostMapping("addTodo")
+    public String addTodo(@RequestParam("todo") String todo){
+        ToDo toDo = new ToDo();
+        toDo.setTodo(todo);
+        toDoRepository.save(toDo);
+        return "redirect:/"; // /으로 자동 이동해라
     }
 }
