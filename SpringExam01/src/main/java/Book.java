75ff55540f079fd63adc8bb0361b4b39a5cac51f
NanNo <inhwanpark19991120@gmail.com>
2024-02-20 22:11:48 +0900

    spring

---
+++ SpringExam01/src/main/java/Book.java
@@ -0,0 +1,24 @@
+public class Book {
+    String title;
+    int price;
+    public Book(String title, int price){
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
