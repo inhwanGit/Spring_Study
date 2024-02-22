418c49d3a482251bc40b70e7536fffe5fa3c0598
NanNo <inhwanpark19991120@gmail.com>
2024-02-22 18:51:12 +0900

    spring bean

--- SpringExam01/src/main/java/exam/ApplicationContext.java
+++ SpringExam01/src/main/java/exam/ApplicationContext.java
@@ -4,25 +4,35 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class ApplicationContext {
     Properties properties;
+    Map objectMap;
     public ApplicationContext() throws IOException {
         properties = new Properties();
+        objectMap = new HashMap<String, Object>();
         properties.load(new FileInputStream("src/main/resources/Beans.properties"));
     }
     public Object getBean(String id) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-        String className = properties.getProperty(id);
-        // class name에 해당하는 문자열을 가지고 인스턴스를 생성한다.
-        // Class.forName(className)은 CLASSPATH부터 className에 해당하는 클래스를 찾은 후
-        // 그 클래스 정보를 반환한다.
-        Class clazz = Class.forName(className);
+
+        Object o1 = objectMap.get(id);
+        if(o1 == null) {
+            String className = properties.getProperty(id);
+            // class name에 해당하는 문자열을 가지고 인스턴스를 생성한다.
+            // Class.forName(className)은 CLASSPATH부터 className에 해당하는 클래스를 찾은 후
+            // 그 클래스 정보를 반환한다.
+            Class clazz = Class.forName(className);
 //        Method[] methods = clazz.getMethods();
 //        for(Method m : methods){
 //            System.out.println(m.getName());
 //        }
-        Object o = clazz.newInstance(); // clazz정보를 이용해 인스턴스를 생성한다.
-        return o;
+            Object o = clazz.newInstance(); // clazz정보를 이용해 인스턴스를 생성한다.
+            objectMap.put(id, o);
+            o1 = objectMap.get(id);
+        }
+        return o1;
     }
 }
--- SpringExam01/src/main/java/exam/ApplicationContextMain.java
+++ SpringExam01/src/main/java/exam/ApplicationContextMain.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class ApplicationContextMain {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         ApplicationContext context = new ApplicationContext();
-        Book book = (Book) context.getBean("book1");
+        Book book = (Book) context.getBean("book1"); // id에 해당하는 인스턴스를 달라.
         book.setPrice(5000);
         book.setTitle("즐거운 자바");
 
